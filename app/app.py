07ce95483d59e0dadbdbdb94c76fcddc9596263b
Rui Constancio <rui.constancio@protonmail.com>
2021-06-20 11:02:52 -0300

    Add delete scheduled job

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ import tornado.web
 
 
 RUNNING_JOBS = {}
+SCHEDULED_CALLBACKS = {}
 JOB_HISTORY = {}
 SCHEDULED_JOBS = {}
 DATE_FORMAT = "%d-%m-%Y %H:%M:%S"
@@ -30,8 +31,10 @@ class JobsHandler(tornado.web.RequestHandler):
     async def post(self):
         job_name = self.get_argument('job_name')
         date = self.get_argument('date')
-        await schedule_job(job_name=job_name, date=date)
-
+        random_id = uuid.uuid4().hex
+        await schedule_job(job_name=job_name, date=date, job_id=random_id)
+        self.write({"message": f"job {job_name} with id {random_id} will be scheduled to run in {date}"})
+    
 class JobDetailHandler(tornado.web.RequestHandler):
     def get(self, job_id):
         if job_id in SCHEDULED_JOBS.keys():
@@ -40,19 +43,30 @@ class JobDetailHandler(tornado.web.RequestHandler):
             self.write(RUNNING_JOBS[job_id])
         else:
             self.write(JOB_HISTORY[job_id])
+    
+    def delete(self, id):
+        SCHEDULED_CALLBACKS[id].cancel()
+        SCHEDULED_CALLBACKS.pop(id)
+        deleted_job = SCHEDULED_JOBS.pop(id)
+        JOB_HISTORY[id] = deleted_job
+        JOB_HISTORY[id]['finish_status'] = 'did_not_run'
+        JOB_HISTORY[id]['finish_date'] = datetime.fromtimestamp(ttime()).strftime(DATE_FORMAT)
+        JOB_HISTORY[id]['status'] = 'canceled'
 
-async def schedule_job(job_name: str, date: str):
-    random_id = uuid.uuid4().hex
+
+async def schedule_job(job_name: str, date: str, job_id: str):
     now = datetime.fromtimestamp(ttime())
-    SCHEDULED_JOBS[random_id] = {
-        "id": random_id,
+    run_time_timestamp = (now + strdate_to_datetime(date)).timestamp()
+    x = tornado.ioloop.IOLoop.instance().call_at(callback=runjob, job_id=job_id, job_name=job_name, when=run_time_timestamp)
+    SCHEDULED_JOBS[job_id] = {
+        "id": job_id,
         "job_name": job_name,
         "scheduled_in": now.strftime(DATE_FORMAT),
         "scheduled_to": (now + strdate_to_datetime(date)).strftime(DATE_FORMAT),
         "status": "scheduled"
     }
-    run_time_timestamp = (now + strdate_to_datetime(date)).timestamp()
-    tornado.ioloop.IOLoop.instance().call_at(callback=runjob, job_id=random_id, job_name=job_name, when=run_time_timestamp)
+    SCHEDULED_CALLBACKS[job_id] = x
+
 
 def runjob(job_name: str, job_id: str):
     delayed_args = partial(delayed, job_name, job_id)
@@ -62,8 +76,8 @@ def delayed(job_name: str, job_id: str):
     running_job = SCHEDULED_JOBS.pop(job_id)
     RUNNING_JOBS[job_id] = running_job
     RUNNING_JOBS[job_id]["status"] = "running"
-    sleep(15)
-    print(job_name)
+    sleep(5)
+    print(f"{job_name} ran...")
     ran_job = RUNNING_JOBS.pop(job_id)
     JOB_HISTORY[job_id] = ran_job
     JOB_HISTORY[job_id]['finish_status'] = 'success'
