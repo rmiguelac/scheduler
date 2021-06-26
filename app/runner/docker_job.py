9ab99145b6b882b276aeec6a0bb6ada36fcb79b9
Rui Constancio <rui.constancio@protonmail.com>
2021-06-26 15:16:54 -0300

    Update docker file

--- app/runner/docker_job.py
+++ app/runner/docker_job.py
@@ -7,10 +7,10 @@ def run_job(image: str, command: dict = '') :
         # We're assuming this api will be running in the server in which docker is running
         client = docker.DockerClient(base_url='unix://var/run/docker.sock')
     except Exception as err:
-        print(f"Something went wrong while trying to stablish connection to docker socket...\n{err}")
+        print(f"[ERROR] Something went wrong while trying to stablish connection to docker socket...\n{err}")
     
     try:
         output = client.containers.run(image, command, detach=True)
         return output
     except Exception as err:
-        print(f"Failed to run job {image}\n{err}")
+        print(f"[ERROR] Failed to run job {image}\n{err}")
